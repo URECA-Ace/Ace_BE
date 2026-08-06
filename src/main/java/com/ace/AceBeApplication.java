68f5532da6d992ccfd08142d4a833714530e041a
sungwon <sw990208@gmail.com>
2026-08-06 15:19:04 +0900

    chore: 최초 환경 설정

---
+++ src/main/java/com/ace/AceBeApplication.java
@@ -0,0 +1,13 @@
+package com.ace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AceBeApplication.class, args);
+	}
+
+}
